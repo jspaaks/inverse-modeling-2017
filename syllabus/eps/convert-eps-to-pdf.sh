c8d9d51de45c97eee5e3dbc9d8692fd107882e96
Jurriaan H. Spaaks <j.spaaks@esciencecenter.nl>
2017-02-20 16:04:34 +0100

    script to generated pdf from eps

---
+++ syllabus/eps/convert-eps-to-pdf.sh
@@ -0,0 +1,12 @@
+epstopdf syllabus-front.eps
+epstopdf diffevo-result.eps
+epstopdf hymod.eps
+epstopdf interceptionmodel-flows.eps
+epstopdf interceptionmodel-mancal-gui.eps
+epstopdf interceptionmodel-subset.eps
+epstopdf pop-res-figures.eps
+epstopdf respsurf-rosenbrock.eps
+epstopdf respsurf-sluginj.eps
+epstopdf syllabus-front.eps
+rm -rf ./converted/ && mkdir converted
+mv *.pdf ./converted/
